499277716570e535af9694390856a4317a9524d4
vlinayo <vlinayo@users.noreply.github.com>
2024-03-31 18:22:07 +0000

    deploy: 48d7cc3d058c03d1b0913afbfe537961e1cb73a0

---
+++ assets/EpisodeDetail-6uUXU3fr.js
@@ -0,0 +1,1 @@
+import{u as t,j as e}from"./index-YBMV14WJ.js";function c(){const i=t(),n=()=>{i("/music-podcasts-challenge/")},s=()=>{i(-1)};return e.jsxs("div",{children:[e.jsx("h2",{children:"EpisodeDetail"}),e.jsx("button",{onClick:n,children:"Go Home"}),e.jsx("button",{onClick:s,children:"Go Back"})]})}export{c as default};
